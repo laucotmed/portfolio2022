eb0feb00aa34bc51eeb87b02f6b5d83b68fdbb07
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-09-20 20:59:41 +0200

    Final Touches and Clean Code

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
 
-    $.fn.scrollReveal(); 
+    /* INITIALIZE MAKE APPEAR CONTENT ON SCROLL */
 
-    // ESCONDER MENÚ AL HACER SCROLL HACIA ABAJO
+    $.fn.scrollReveal();
+
+    /* HIDE MENU WHEN YOU SCROLL DOWN */
+
     var lastScrollTop = 0;
     var headerHeight = $('header').outerHeight();
     var menuHeight = $(".menu").outerHeight();
@@ -11,41 +14,35 @@ $(document).ready(function () {
 
         var st = $(this).scrollTop();
 
-        /* if ($("#burger_menu").css("display") === "none") { */
+        // Scroll Down
+        if (st > lastScrollTop && st > menuHeight) {
+            $(".menu").slideUp(400);
 
-            // Scroll Down
-            if (st > lastScrollTop && st > menuHeight) {
-                $(".menu").slideUp(400);
-
-                // Scroll Up
-            } else if (st + $(window).height() < $(document).height()) {
-                $(".menu").slideDown(400);
-            }
+            // Scroll Up
+        } else if (st + $(window).height() < $(document).height()) {
+            $(".menu").slideDown(400);
+        }
 
 
-            if (st < headerHeight) {
-                $('.menu').removeClass('solid');
-            } else {
-                $('.menu').addClass('solid');
-            }
+        if (st < headerHeight) {
+            $('.menu').removeClass('solid');
+        } else {
+            $('.menu').addClass('solid');
+        }
 
-            lastScrollTop = st;
-
-        /* } */
+        lastScrollTop = st;
 
     });
 
-    /* MENÚ HAMBURGUESA */
-    /* Hacemos un evento en el que al hacer click en nuestro icono de hamburguesa, las distintas clases se añadan a las líneas que lo forman y se produce una pequeña animación para convertirlas en una X y viceversa al usar toggleClass.  */
+    /* BURGER MENU */
 
     $("#burger_menu").click(function () {
 
-        /* Animación de las barras añadiendo las clases en CSS en las que se modifican la rotación y opacidad */
+
         $("#burger_menu>span:nth-child(1)").toggleClass("first")
         $("#burger_menu>span:nth-child(2)").toggleClass("second")
         $("#burger_menu>span:nth-child(3)").toggleClass("third")
 
-        /* Despliego del menú en móvil añadiendo la clase CSS show. En caso de hacer clic en la "X", se desactivaría, cerrando el menú. Usamos stop() para evitar que se forme una cola de acciones si se hace clics muy rápidamente y seguidos. También añadimos la clase "overflow-menu" al body, evitando que podamos hacer scroll mientras el menú se encuentre abierto. */
         $(".menu_items").stop()
         $(".menu_items").toggleClass("show")
         $('body').toggleClass("overflow-menu");
@@ -59,6 +56,6 @@ $(document).ready(function () {
             $("#burger_menu>span:nth-child(2)").toggleClass("segunda");
             $("#burger_menu>span:nth-child(3)").toggleClass("tercera");
         }
-    }) 
+    })
 
 })
