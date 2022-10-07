077ec57a3a9ee44783ec1a42a07b5d09eddff530
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-10-07 13:51:03 +0200

    Translated Index

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -40,6 +40,8 @@ $(document).ready(function () {
 
     //Spanish
     $(".es").click(function () {
+
+        //FadeOut Animation
         $('body').animate({
             'opacity': '0.0'
         }, 600, function () {
@@ -49,16 +51,49 @@ $(document).ready(function () {
             $(".menu_item:nth-of-type(3) a").text("//Portfolio");
             $(".menu_item:nth-of-type(4) a").text("//Contacto");
 
+            //Flag Change
+            $(".selected-lang img").attr("src", "./src/media/esp.png")
 
+            //Header
+            $("#home h2").text("DESARROLADORA WEB FRONT END");
 
-            $(".selected-lang img").attr("src", "https://cdn.countryflags.com/thumbs/spain/flag-800.png")
-            /* $(".language-selected").removeClass("change-es");
-            $(".language-selected").addClass("change-en"); */
             //About
-            $("#about h2:nth-child(1)").text("//SOBRE MÍ");
-            //About
-            $("#profile_description p").html("¡Hola! Me llamo <span>Laura Cote Medina</span>. Soy una desalloradora web de España, apasionada del <span>Web Design</span> y con ganas de adquirir más experiencia profesional, preferentemente en el ámbito del <span>FrontEnd</span>. Siempre estoy dispuesta a aprender tecnologías y metodologías nuevas. Se me da bien trabajar en equipo, siendo también independiente a la hora de resolver problemas. ");
+            $("#about h2:nth-of-type(1)").text("//SOBRE MÍ");
 
+            $("#profile_description p").html("¡Hola! Me llamo <span>Laura Cote Medina</span>. Soy una desalloradora web de España, apasionada del <span>Diseño Web</span> y con ganas de adquirir más experiencia profesional, preferentemente en el ámbito del <span>FrontEnd</span>. Siempre estoy dispuesta a aprender tecnologías y metodologías nuevas. Se me da bien trabajar en equipo, siendo también independiente a la hora de resolver problemas. ");
+            $("#profile_description a").text("Descargar CV");
+
+            $("#about h2:nth-of-type(2)").text("Herramientas y Tecnologías");
+
+            //Portfolio
+
+            //Ori
+            $(".project:nth-of-type(1) p").html('Página web inspirada en el videojuego "Ori & The Blind Forest". En la sección <span>Skills</span> usé <span>PHP</span>, <span>JQuery</span> y <span>AJAX</span> para extraer de una base de datos las distintas habilidades del protagonista y mostrarlas de forma dinámica mediante un carrusel.');
+            $(".project:nth-of-type(1) a:nth-of-type(2)").text('ENLACE A LA WEB');
+
+            //Katnip
+            $(".project:nth-of-type(2) p").html('Este proyecto se realizó empleando <span>WordPress</span> y algunos plugins como <span>Elementor Pro</span>. La temática elegida fue <span>"cat cafe"</span>, así que la página web fue diseñada para atraer tanto a posibles clientes de la cafetería como a adoptantes.');
+            $(".project:nth-of-type(2) a:nth-of-type(2)").text('ENLACE A LA WEB');
+
+            //Korean Food
+            $(".project:nth-of-type(3) p").html('Esta página web fue creada desde cero usando <span>HTML5</span>, <span>CSS3</span> y <span>JavaScript</span>. Utilizamos un <span>scroll horizontal</span> para recorrer un poco de la historia de la <span>gastronomía surcoreana</span> y unas pocas recetas.');
+            $(".project:nth-of-type(3) a:nth-of-type(2)").text('ENLACE A LA WEB');
+
+            //Tourning
+            $(".project:nth-of-type(4) p").html('Diseño de una <span>app para viajes</span> en la que el itinerario del usuario cambiara según las condiciones climáticas. Se empleó la metodología ágil de <span>"Design Thinking"</span> para todo el proceso de investigación y diseño de <span>UX/UI</span> y se realizó un prototipo en <span>Figma</span>.');
+            $(".project:nth-of-type(4) a:nth-of-type(2)").text('ENLACE AL PROYECTO');
+
+            //Contact
+            $("#contact h2").text("//CONTACTO");
+            $("#contact_text p").html('¿Tienes alguna <span>pregunta</span> o quieres <span>ponerte en contacto conmigo</span>? ¡No dudes en <span>usar este formulario</span> o contactarme a través de cualquiera de mis <span>redes</span> y responderé lo más rápido posible!');
+
+            $(".contactForm form label:nth-of-type(1)").text('Nombre');
+            $(".contactForm form label:nth-of-type(2)").text('Correo electrónico');
+            $(".contactForm form label:nth-of-type(3)").text('Mensaje');
+            $("#message").attr("placeholder", "¿En qué puedo ayudarte?")
+            $("#submitButton").text('Enviar');
+
+            //FadeIn Animation
             $('body').animate({
                 'opacity': '1.0'
             }, 600);
@@ -66,7 +101,7 @@ $(document).ready(function () {
     });
 
     //English
-   /*  WIP */
+    /*  WIP */
 
     /* BURGER MENU */
 
