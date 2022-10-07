1f5465e4a0d2b62923e59672a72f1da483e5babb
Laura Cote Medina <lauracotemedina@alumnos.cei.es>
2022-10-07 18:04:57 +0200

    Added English Translation

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
             //Main Menu
             $(".menu_item:nth-of-type(1) a").text("//Inicio");
             $(".menu_item:nth-of-type(2) a").text("//Sobre Mí");
-            $(".menu_item:nth-of-type(3) a").text("//Portfolio");
+           /*  $(".menu_item:nth-of-type(3) a").text("//Portfolio"); */
             $(".menu_item:nth-of-type(4) a").text("//Contacto");
 
             //Flag Change
@@ -101,7 +101,66 @@ $(document).ready(function () {
     });
 
     //English
-    /*  WIP */
+    $(".en").click(function () {
+
+        //FadeOut Animation
+        $('body').animate({
+            'opacity': '0.0'
+        }, 600, function () {
+            //Main Menu
+            $(".menu_item:nth-of-type(1) a").text("//HOME");
+            $(".menu_item:nth-of-type(2) a").text("//ABOUT");
+            /* $(".menu_item:nth-of-type(3) a").text("//PORTFOLIO"); */
+            $(".menu_item:nth-of-type(4) a").text("//CONTACT");
+
+            //Flag Change
+            $(".selected-lang img").attr("src", "./src/media/uk.png")
+
+            //Header
+            $("#home h2").text("FRONT END WEB DEVELOPER");
+
+            //About
+            $("#about h2:nth-of-type(1)").text("//ABOUT");
+
+            $("#profile_description p").html("Hi! My name is <span>Laura Cote Medina</span>. I'm a web developer from Spain who is passionate about <span>Web Design</span> and eager to obtain more professional experience, preferably on <span>FrontEnd</span>. I'm always willing to learn other technologies and tools and I'm good at working in a team following agile methodologies, while being very independent when solving problems alone.");
+            $("#profile_description a").text("Download CV");
+
+            $("#about h2:nth-of-type(2)").text("Tools & Technologies");
+
+            //Portfolio
+
+            //Ori
+            $(".project:nth-of-type(1) p").html('Website inspired by the videogame Ori & The Blind Forest. In the <span>Skills</span> section , we used <span>PHP</span>, <span>JQuery</span> and also <span>AJAX</span> to extract the data of the proganist’s skills from a database and show them dynamically using a carousel.');
+            $(".project:nth-of-type(1) a:nth-of-type(2)").text('LINK TO WEBSITE');
+
+            //Katnip
+            $(".project:nth-of-type(2) p").html('This project was made using <span>WordPress</span> and some plugins like <span>Elementor Pro</span>. The chosen theme was <span>"cat cafe"</span>, so the website was designed to attract both customers and potential adopters.');
+            $(".project:nth-of-type(2) a:nth-of-type(2)").text('LINK TO WEBSITE');
+
+            //Korean Food
+            $(".project:nth-of-type(3) p").html('This website was built from scratch using <span>HTML5</span>, <span>CSS3</span> and <span>JavaScript</span>. A <span>horizontal scroll</span> is used to go through a bit of the history of <span>South Korean food</span> and a few recipes.');
+            $(".project:nth-of-type(3) a:nth-of-type(2)").text('LINK TO WEBSITE');
+
+            //Tourning
+            $(".project:nth-of-type(4) p").html('Designed a <span>travel app</span> where your itinerary changed according to the weather. <span>Design Thinking</span> was the methodology used for the entire <span>UX/UI</span> process, and then a prototype was made with <span>Figma</span>.');
+            $(".project:nth-of-type(4) a:nth-of-type(2)").text('LINK TO PROJECT');
+
+            //Contact
+            $("#contact h2").text("//CONTACTO");
+            $("#contact_text p").html('Do you have any <span>question</span> or want to <span>get in touch with me</span>? Feel free to use this <span>form</span> or reach me on any of my <span>networks</span> and I will get back to you as soon as I can!');
+
+            $(".contactForm form label:nth-of-type(1)").text('Name');
+            $(".contactForm form label:nth-of-type(2)").text('Email');
+            $(".contactForm form label:nth-of-type(3)").text('Message');
+            $("#message").attr("placeholder", "How can I help you?")
+            $("#submitButton").text('Send');
+
+            //FadeIn Animation
+            $('body').animate({
+                'opacity': '1.0'
+            }, 600);
+        });
+    });
 
     /* BURGER MENU */
 
